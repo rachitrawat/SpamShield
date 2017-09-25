1cc43721a8ba641a7c9de04aab2c8c1aa7b02ca9
rachitrawat <rachit.rc.rawat@gmail.com>
2017-09-25 15:23:30 +0530

    Plot 10 most common tags
    Signed-off-by: rachitrawat <rachit.rc.rawat@gmail.com>

--- code/IR-Notebook.py
+++ code/IR-Notebook.py
@@ -3,12 +3,25 @@
 
 # ## Setup Environment
 
-# In[ ]:
+# In[1]:
 
 # load required packages
 
 # for creating dataframes from csv datasets
 import pandas as pd
+
+# for counting
+import collections
+
+# for scientific computing
+import numpy as np
+import math
+
+# for plotting graphs
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+# magic function
+get_ipython().magic('matplotlib inline')
 
 # kaggle - data set files are available in the "../input/" directory
 dataset_dir = "../input/"
@@ -36,4 +49,27 @@ tags_df = pd.read_csv(dataset_dir+dataset_dir_tags, encoding='latin1')
 # questions_df
 # answers_df
 # tags_df
+
+
+# ## Top 10 most common tags
+
+# In[ ]:
+
+tags_tally = collections.Counter(tags_df['Tag'])
+
+# x = tag name, y = tag frequency
+x, y = zip(*tags_tally.most_common(10))
+
+colormap = plt.cm.gist_ncar #nipy_spectral, Set1,Paired  
+colors = [colormap(i) for i in np.linspace(0, 0.8,50)]   
+
+area = [i/5000 for i in list(y)]   # 0 to 15 point radiuses
+plt.figure(figsize=(8,8))
+plt.ylabel("Frequency")
+plt.title("Top 10 most common tags")
+for i in range(len(y)):
+        plt.plot(i,y[i], marker='v', linestyle='',ms=area[i],label=x[i])
+
+plt.legend(numpoints=1)
+plt.show()
 
