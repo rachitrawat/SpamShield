91680a04c3b066b4dff234b9976c9930b7687395
rachitrawat <rachit.rc.rawat@gmail.com>
2017-10-29 13:52:11 +0530

    Strip html tags from text body
    Signed-off-by: rachitrawat <rachit.rc.rawat@gmail.com>

--- code/IR-Notebook.py
+++ code/IR-Notebook.py
@@ -9,13 +9,16 @@
 
 # ** Setup Environment **
 
-# In[ ]:
+# In[2]:
 
 
 # load required packages
 
 # for creating dataframes from csv datasets
 import pandas as pd
+
+# for removing HTML tags from text body
+from html.parser import HTMLParser
 
 # for counting
 import collections
@@ -44,23 +47,58 @@ from subprocess import check_output
 print(check_output(["ls", dataset_dir]).decode("utf8"))
 
 
-# ** pandas - load CSV into dataframe **
+# ** HTML tags Stripper class **
 
-# In[ ]:
+# In[3]:
+
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.strict = False
+        self.convert_charrefs= True
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
+
+# ** pandas - load CSV into dataframe **
+# 
+
+# In[4]:
 
 
 # Read CSV
 
-# Dimensionality - (rows, columns)
+# Original Dimensionality - (rows, columns)
 
 # (1264216, 7) 
-questions_df = pd.read_csv(dataset_dir+dataset_dir_questions, encoding='latin1')
+# Columns (Id, OwnerUserId, CreationDate, ClosedDate, Score, Title, Body)
+# frame every 100th question (resource restraints)
+questions_df = pd.read_csv(dataset_dir+dataset_dir_questions, encoding='latin1').iloc[::100, :]
 
 # (2014516, 6)
-answers_df = pd.read_csv(dataset_dir+dataset_dir_answers, encoding='latin1')
+# Columns (Id, OwnerUserId, CreationDate, ParentId, Score, Body)
+# frame every 100th answer (resource restraints)
+answers_df = pd.read_csv(dataset_dir+dataset_dir_answers, encoding='latin1').iloc[::100, :]
 
 # (3750994, 2)
-tags_df = pd.read_csv(dataset_dir+dataset_dir_tags, encoding='latin1')
+# Columns (Id, Tag)
+# frame every 100th tag (resource restraints)
+tags_df = pd.read_csv(dataset_dir+dataset_dir_tags, encoding='latin1').iloc[::100, :]
+
+
+# ** Sample dataframe before stripping **
+
+# In[5]:
+
 
 # Calculate dimensionality
 # questions_df.shape 
@@ -68,14 +106,46 @@ tags_df = pd.read_csv(dataset_dir+dataset_dir_tags, encoding='latin1')
 # tags_df.shape 
 
 # Sample dataframe - uncomment to view
-# questions_df.head(10) 
+questions_df.head(10) 
 # answers_df.head(10)
 # tags_df.head(10) 
 
 
+# ** Strip HTML tags from text body **
+
+# In[6]:
+
+
+# Remove HTML tags from body column
+for index, row in questions_df.iterrows():
+   questions_df.at[index, 'Body']= strip_tags(row[6])
+   
+# Remove HTML tags from body column
+for index, row in answers_df.iterrows():
+   answers_df.at[index, 'Body']= strip_tags(row[5])  
+
+
+# ** Sample dataframe after stripping **
+
+# In[7]:
+
+
+# Calculate dimensionality
+# questions_df.shape 
+# answers_df.shape 
+# tags_df.shape 
+
+# Sample dataframe - uncomment to view
+questions_df.head(10) 
+# answers_df.head(10)
+# tags_df.head(10) 
+
+
+# # Initial analysis
+
 # ** Top 10 most common tags **
 
-# In[ ]:
+# In[14]:
 
 
 tags_tally = collections.Counter(tags_df['Tag'])
@@ -86,7 +156,7 @@ x, y = zip(*tags_tally.most_common(10))
 colormap = plt.cm.gist_ncar #nipy_spectral, Set1,Paired  
 colors = [colormap(i) for i in np.linspace(0, 0.8,50)]   
 
-area = [i/5000 for i in list(y)]   # 0 to 15 point radiuses
+area = [i/50 for i in list(y)]   # 0 to 15 point radiuses
 plt.figure(figsize=(8,8))
 plt.ylabel("Frequency")
 plt.title("Top 10 most common tags")
@@ -97,22 +167,22 @@ plt.legend(numpoints=1)
 plt.show()
 
 
-# ** Distribution  - number of answers per question **
+# ![](http://)![](http://)**Distribution  - number of answers per question**
 
-# In[ ]:
+# In[95]:
 
 
 ans_per_question = collections.Counter(answers_df['ParentId'])
 answerid,noAnswers= zip(*ans_per_question.most_common())
 
 N=50
-plt.bar(range(N), noAnswers[:N], align='center', alpha=0.5)
+plt.bar(range(N), noAnswers[:N], align='center', alpha=0.7)
 #plt.xticks(y_pos, objects)
 
 plt.ylabel('Number of Answers per Question')
 plt.xlabel('Question Id')
 plt.title('Distribution of Answers per question ')
-plt.text(3,400,"Average answers per question: "+str(math.ceil((np.mean(noAnswers)))))
+plt.text(10,4,"Average answers per question: "+str(math.floor((np.mean(noAnswers)))))
 
 plt.show()
 
